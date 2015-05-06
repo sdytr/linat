3712487b6aa590ddaa421a7cc75f647cb5f3cdbf
sdytr <dm@billo.systems>
2015-05-06 15:19:40 -0500

    Added support for API docs.

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 PROJECT = linat
 
 include resources/make/common.mk
+include resources/make/docs.mk
---
+++ resources/make/docs.mk
@@ -0,0 +1,25 @@
+.PHONY: docs
+REPO = $(shell git config --get remote.origin.url)
+DOCS_DIR = docs
+DOCS_BUILD_DIR = $(DOCS_DIR)/build
+SLATE_GIT_HACK = $(DOCS_DIR)/.git
+
+$(SLATE_GIT_HACK):
+	cd $(DOCS_DIR) && ln -s ../.git .
+
+docs-setup:
+	cd docs && bundle install
+
+devdocs: $(SLATE_GIT_HACK)
+	cd docs && bundle exec middleman server
+
+docs: $(SLATE_GIT_HACK)
+	cd docs && rake build
+
+publish: docs
+	rm -rf $(DOCS_BUILD_DIR)/.git
+	cd $(DOCS_BUILD_DIR) && \
+	git init && \
+	git add * &> /dev/null && \
+	git commit -a -m "Generated content." &> /dev/null && \
+	git push -f $(REPO) master:gh-pages
